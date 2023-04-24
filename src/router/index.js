7a9741c53a9071d99899b7f825a493f0e21c1797
litao <2532566074@qq.com>
2023-04-24 12:07:36 +0800

    feat(custom): 全局路由拦截实现登录情况判断

--- src/router/index.js
+++ src/router/index.js
@@ -24,4 +24,27 @@ const router = createRouter({
   ]
 })
 
+router.beforeEach(async (to, from, next) => {
+  const token = getToken()
+  // 没有登录，强制跳转回登录页
+  if (!token && to.path != '/login') {
+    toast('请先登录', 'error')
+    return next({ path: '/login' })
+  }
+
+  // 防止重复登录
+  if (token && to.path == '/login') {
+    toast('请勿重复登录', 'error')
+    return next({ path: from.path ? from.path : '/' })
+  }
+
+  const { getStoreInfo } = useAdminStore()
+  //  如果登录了，就获取用户信息，存储到 pinia
+  if (token) {
+    await getStoreInfo()
+  }
+
+  next()
+})
+
 export default router
