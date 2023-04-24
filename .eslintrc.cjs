53d964b08201c6aa99cf10920d6909c737d1bf91
litao <2532566074@qq.com>
2023-04-24 10:56:00 +0800

    feat(custom): 项目搭建+代码规范

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  // 表示当前目录即为根目录，ESLint 规则将被限制到该目录下
+  root: true,
+  env: {
+    // 在 node 环境下启动 ESLint 检测
+    node: true
+  },
+  // ESLint 中基础配置需要继承的配置：vue3 的必要和标准配置
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  /**
+   * 需要修改的启用规则及其各自的错误级别
+   * 错误级别分为三种：
+   * "off" 或 0 - 关闭规则
+   * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+   * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+   */
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
